865d923211e83f9810612f10eb9cdcd6744fb10f
maciej <f171a9a3497c8b@users.noreply.github.com>
2018-04-12 17:22:28 +0200

    Update binary_genetic_algorithm.py
    List of changes:
    -- add ability to pass additional argument(s) to fitness function
    -- fitness function no longer need to return probabilities
    -- selecting chromosomes to next generation is based on relative probabilities
    calculated from scores and computed inside select function
    -- rename fit_values to scores
    -- move elements common for each generation of chromosomes (indexes array
    from select function, bits array from crossover function) to run function
    and add them as arguments to functions
    -- run function returns only a chromosome (1D numpy Boolean array)
    -- number of iterations (argument in run function) has no longer default value
    -- revise docstrings

--- binary_genetic_algorithm.py
+++ binary_genetic_algorithm.py
@@ -10,12 +10,14 @@ def generate(pop_size, chrom_length):
     '''
     Inputs:
 
-    pop_size -- positive integer number
-             -- total number of chromosomes in a generation
+    pop_size
+    -- positive integer number
+    -- total number of chromosomes in a generation
 
-    chrom_length -- positive integer number
-                 -- number of bits in a chromosome
-                 -- length of a chromosome
+    chrom_length
+    -- positive integer number
+    -- number of bits in a chromosome
+    -- length of a chromosome
 
 
     Each chromosome is represented as a fixed length 1D numpy array
@@ -30,20 +32,26 @@ def generate(pop_size, chrom_length):
     of chromosomes (solution candidates).
     '''
 
-    assert pop_size < (2 ** chrom_length)
+    assert pop_size < (1 << chrom_length)  # 2 ** chrom_length
 
     return np.random.randint(0, 2, size=(pop_size, chrom_length), dtype=np.bool)
 
 
-def select(population, fit_values):
+def select(population, scores, indexes):
     '''
     Inputs:
 
-    population -- array of chromosomes
-               -- each chromosome must be represented as 1D numpy boolean array
+    population
+    -- array of chromosomes
+    -- each chromosome must be represented as 1D numpy boolean array
 
-    fit_values -- array of fitness values corresponding to chromosomes
-               -- each fitness value must be a non-negative real number
+    scores
+    -- array of fitness values corresponding to chromosomes
+    -- each fitness value must be a non-negative real number
+
+    indexes
+    -- 1D numpy integer array
+    -- indexes of chromosomes in the population (row index)
 
 
     Selection is based on the roulette wheel method (fitness proportionate selection)
@@ -57,10 +65,9 @@ def select(population, fit_values):
     (solution candidates) chosen with repetition from a given population.
     '''
 
-    # Enumerate chromosomes in the population
-    indexes = np.arange(population.shape[0], dtype=np.uint)
+    probabilities = scores / np.sum(scores)
 
-    indexes = np.random.choice(indexes, size=indexes.size, replace=True, p=fit_values)
+    indexes = np.random.choice(indexes, size=indexes.size, replace=True, p=probabilities)
 
     np.random.shuffle(indexes)
 
@@ -71,12 +78,14 @@ def mutate(population, mut_prob):
     '''
     Inputs:
 
-    population -- array of chromosomes
-               -- each chromosome must be represented as 1D numpy boolean array
+    population
+    -- array of chromosomes
+    -- each chromosome must be represented as 1D numpy boolean array
 
-    mut_prob -- positive real number
-             -- mutation rate
-             -- probability that a bit will be inverted
+    mut_prob
+    -- positive real number
+    -- mutation rate
+    -- probability that a bit will be inverted
 
 
     Mutation can occur independently at every bit along each chromosome
@@ -93,18 +102,24 @@ def mutate(population, mut_prob):
     return population ^ bits_to_mutate
 
 
-def crossover(population, crs_prob):
+def crossover(population, crs_prob, bits):
     '''
     Inputs:
 
-    population -- array of chromosomes
-               -- each chromosome must be represented as 1D numpy boolean array
-               -- number of chromosomes must be even
+    population
+    -- array of chromosomes
+    -- each chromosome must be represented as 1D numpy boolean array
+    -- number of chromosomes must be even
 
-    crs_prob -- positive real number
-             -- crossover (recombination) probability
-             -- probability that a pair of chromosomes will exchange
-                part of bit sequences
+    crs_prob
+    -- positive real number
+    -- crossover (recombination) probability
+    -- probability that a pair of chromosomes will exchange
+       part of bit sequences
+
+    bits
+    -- 1D numpy integer array
+    -- indexes of bits in a chromosome (column index)
 
 
     Commute part of binary sequences between paired chromosomes.
@@ -118,7 +133,9 @@ def crossover(population, crs_prob):
     # Each column represents specific bit
 
     # Get the number of pairs and the length of sequences
-    rows, cols = population[0::2].shape
+    rows, cols = population.shape
+
+    rows >>= 1  # rows //= 2
 
     # Select pairs of chromosomes for which sequences of bits will be exchanged
     pairs = np.random.uniform(size=(rows, 1)) < crs_prob
@@ -127,9 +144,6 @@ def crossover(population, crs_prob):
 
     # Set a single crossover bit for each pair of chromosomes
     breakpoints = np.random.randint(1, cols, size=(rows, 1), dtype=np.uint)
-
-    # Enumerate bits
-    bits = np.arange(cols, dtype=np.uint)
 
     # Divide each sequence of bits into two parts
     positions = bits < breakpoints
@@ -143,49 +157,60 @@ def crossover(population, crs_prob):
     ))
 
 
-def run(fitness,
+def run(fit_func,
         crs_prob,
         mut_prob,
         chrom_length,
-        pop_size=100,
-        iterations=200,
-        threshold=1):
+        pop_size,
+        iterations,
+        fit_args=None,
+        threshold=1.0):
     '''
     Inputs:
 
-    fitness -- cost function
-            -- must take one positional argument: nested (2D) numpy boolean array
-            -- must return 1D numpy array of real numbers between
-               0 (invalid sequence) and 1 (perfect fitness)
+    fit_func
+    -- fitness function
+    -- first positional argument must be a nested (2D) numpy boolean array
+    -- must return 1D numpy array with real numbers between
+       0 (invalid sequence) and 1 (perfect fitness) and size
+       equal to the number of rows of the given numpy array
 
-    crs_prob -- positive real number
-             -- crossover (recombination) probability
-             -- probability that a pair of chromosomes will exchange
-                part of bit sequences
+    crs_prob
+    -- positive real number
+    -- crossover (recombination) probability
+    -- probability that a pair of chromosomes will exchange
+       part of bit sequences
 
-    mut_prob -- positive real number
-             -- mutation rate
-             -- probability that a bit will be inverted
+    mut_prob
+    -- positive real number
+    -- mutation rate
+    -- probability that a bit will be inverted
 
-    chrom_length -- positive integer number
-                 -- number of bits in a chromosome
-                 -- length of a chromosome
+    chrom_length
+    -- positive integer number
+    -- number of bits in a chromosome
+    -- length of a chromosome
 
-    pop_size -- positive integer number
-             -- must be even
-             -- total number of chromosomes in a generation
-             -- default is 100
+    fit_args
+    -- list, tuple, or None (default)
+    -- additional argument(s) require by fitness function
 
-    iterations -- positive integer number
-               -- maximum number of generations
-               -- default is 200
+    pop_size
+    -- positive integer number (default is 100)
+    -- must be even
+    -- total number of chromosomes in a generation
 
-    threshold -- positive number less than or equal to 1
-              -- minimum satisfactory fitness level
-              -- higher value is more strict
-              -- default is 1, which produces the highest fitness value after
-                 a given number of iterations (preferably a perfect fitness
-                 or an exact match)
+    iterations
+    -- positive integer number (default is 200)
+    -- maximum number of generations
+
+    threshold
+    -- positive number less than or equal to 1
+    -- minimum satisfactory fitness level
+    -- higher value is more strict
+    -- default is 1, which produces the highest fitness value after
+       a given number of iterations (preferably a perfect fitness
+       or an exact match)
 
 
     The fitness function operates on numpy arrays: for a given population
@@ -199,8 +224,7 @@ def run(fitness,
     condition is not satisfied within a given number of generations.
 
 
-    Returns a tuple with two elements: chromosome (1D numpy boolean array)
-    and its fitness value (positive real number).
+    Returns a chromosome (1D numpy boolean array).
     '''
 
     assert (pop_size % 2) is 0
@@ -208,35 +232,42 @@ def run(fitness,
     assert (mut_prob > 0) and (mut_prob < 1)
     assert (threshold >= 0) and (threshold <= 1)
 
+    if fit_args is None:
+        fit_args = []
+
     # Create initial population and calculate corresponding fitness values
     population = generate(pop_size, chrom_length)
-    fit_values = fitness(population)
+    scores = fit_func(population, *fit_args)
 
-    # Find the best candidate from a current generation
-    j = np.argmax(fit_values)
-    chromosome = population[j]
-    score = fit_values[j]
+    # Find the best candidate from current generation
+    alpha = np.argmax(scores)
+    alpha_chromosome = population[alpha]
+    alpha_score = scores[alpha]
+
+    # Enumerate chromosomes in the population (create rows index)
+    indexes = np.arange(pop_size, dtype=np.uint)
+
+    # Enumerate bits in a chromosome (create columns index)
+    bits = np.arange(chrom_length, dtype=np.uint)
 
     # Create successive generations
     for _ in range(iterations):
 
-        if score >= threshold:
+        if alpha_score >= threshold:
             break
 
         # Recreate population
-        population = select(population, fit_values)
-
-        population = crossover(population, crs_prob)
-
+        population = select(population, scores, indexes)
+        population = crossover(population, crs_prob, bits)
         population = mutate(population, mut_prob)
 
         # Recalculate fitness values
-        fit_values = fitness(population)
+        scores = fit_func(population, *fit_args)
 
-        j = np.argmax(fit_values)
+        alpha = np.argmax(scores)
 
-        if score < fit_values[j]:
-            chromosome = population[j]
-            score = fit_values[j]
+        if alpha_score < scores[alpha]:
+            alpha_chromosome = population[alpha]
+            alpha_score = scores[alpha]
 
-    return (chromosome, score)
+    return alpha_chromosome
